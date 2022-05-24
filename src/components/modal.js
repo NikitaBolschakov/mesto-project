3bea3953f2d9bca564f8cbcf7e16cde7fba99476
RomanMitaki <r.mitaki@yandex.ru>
2022-05-24 22:04:41 +0300

    feat/popup draft base

--- src/components/modal.js
+++ src/components/modal.js
@@ -1,3 +1,4 @@
+/*
 const openPopup = (popup) => {
   popup.classList.add("popup_opened");
   document.addEventListener("keydown", handleClickOnEscape);
@@ -26,3 +27,57 @@ const handleClickOnOverlay = (evt) => {
 };
 
 export { openPopup, closePopup };
+*/
+class Popup {
+  constructor(selector) {
+    this._selector = selector;
+  }
+
+  openPopup(popup) {
+    popup.classList.add("popup_opened");
+    this._selector.addEventListener("keydown", this._handleClickOnEscape);
+  }
+
+  closePopup(popup) {
+    popup.classList.remove("popup_opened");
+    this._selector.removeEventListener("keydown", this._handleClickOnEscape);
+    this._selector.removeEventListener("click", this._handleClickOnOverlay);
+  }
+
+  _handleClickOnEscape(evt) {
+    if (evt.key === "Escape") {
+      //нахожу открытый в данный момент попап
+      const popupOpened = document.querySelector(".popup_opened");
+      this.closePopup(popupOpened);
+    }
+  }
+
+  _handleClickOnOverlay(evt) {
+    const popupOpened = document.querySelector(".popup_opened");
+    if (evt.target === popupOpened) {
+      this.closePopup(popupOpened);
+    }
+  }
+
+  setEventListeners(btn) {
+    btn.addEventListener("click", function () {
+      this.closePopup(this._selector);
+    });
+    this._selector.addEventListener("click", this._handleClickOnOverlay);
+  }
+}
+
+class PopupWithImage extends Popup {
+  constructor(selector) {
+    super(selector);
+  }
+
+openPopup(popup) {
+  popup.setAttribute('src', //this._link);
+  popup.setAttribute('alt', //this._name);
+  popup.textContent = //this._name;
+  super.openPopup(popup);
+
+}
+
+}
